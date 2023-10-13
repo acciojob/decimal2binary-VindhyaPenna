308c305074478678906c923fdcdd64d1ba73acc4
VindhyaPenna <118455671+VindhyaPenna@users.noreply.github.com>
2023-10-13 08:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  let ans = "";
+	while(num>1){
+		ans+=Math.floor(num%2);
+		num = num/2;
+	}
+	if(num==1)
+		ans+=1;
+    ans= reverseString(ans);
+    console.log(ans);
 }
 
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+decimalToBinary(33);
 window.decimalToBinary = decimalToBinary;
